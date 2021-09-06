d070f43239cf6f22a2e24cf2c5a0c1525dd0b3d7
Near <81856011+NEar360@users.noreply.github.com>
2021-09-06 21:12:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+const Bodies=Matter.Bodies;
+const World =Matter.World;
+const Engine=Matter.Engine;
+
+
+var engine, world;
+var bg;
+var snow=[];
+
+function preload(){
+bg=loadImage("snow3.jpg");
+bgmusic = loadSound("Old Memory -  Yosuga no Sora OST.mp3")
+}
+
+function setup() {
+  var canvas = createCanvas(800,400);
+    engine = Engine.create();
+    world = engine.world;
+  bgmusic.loop();
+  bgmusic.play();
+}
+
+
+function draw() {
+  background(bg);  
+  Engine.update(engine);
+  //bgmusic.play();
+  if(frameCount%5==0){
+    snow.push(new Snow(random(0,800),-20,30));
+  }
+for(var i=0;i<snow.length;i++){
+  snow [i].display();
+}
+}
